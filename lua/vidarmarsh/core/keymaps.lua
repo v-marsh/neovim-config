7274200b7313e0578cc821d6d612151bfee04944
v-marsh <vidarmarsh@gmail.com>
2024-01-17 09:42:09 +0000

    Remapped arrow keys to avoid using

--- lua/vidarmarsh/core/keymaps.lua
+++ lua/vidarmarsh/core/keymaps.lua
@@ -4,16 +4,27 @@ vim.g.mapleader = " "
 -- Set keymap group prefixes
 local fileleader = "<leader>f"
 
+-- Disable arrow keys and backspace to aid learning 
+vim.keymap.set({"i", "n"}, "<Right>", "")
+vim.keymap.set({"i", "n"}, "<Up>", "")
+vim.keymap.set({"i", "n"}, "<Left>", "")
+vim.keymap.set({"i", "n"}, "<Down>", "")
+
 -- General keymaps
 vim.keymap.set("i", "jk", "<ESC>")
 
 vim.keymap.set("n", "<leader>nh", ":nohl<CR>")
+
 
 -- Remap all window commands to use a nicer prefix
 vim.keymap.set("n", "<leader>w", "<C-W>")
 
 -- File search keymappings
 vim.keymap.set("n", fileleader .. "f", ":Ex<CR>")
+
+-- Bracket and braces keymappings
+vim.keymap.set("i", "{", "{}<ESC>i")
+vim.keymap.set("i", "{}", "{}")
 
 -- Keymap for changing text wrapping with <A-z>
 vim.keymap.set("n", "<A-z>", function()
@@ -25,3 +36,4 @@ vim.keymap.set("n", "<A-z>", function()
         vim.opt.wrap = true
     end
 end)
+
