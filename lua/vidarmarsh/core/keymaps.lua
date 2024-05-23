6b9460df40de2f177a9c3e65cc87d40f93ddb083
v-marsh <vidarmarsh@gmail.com>
2024-05-23 15:50:28 +0100

    Fixed text wrap
    Fixed text wrap by ensuring that only whole words are wrapped and
    indentation is preserved for wrapped lines

--- lua/vidarmarsh/core/keymaps.lua
+++ lua/vidarmarsh/core/keymaps.lua
@@ -4,14 +4,14 @@ vim.g.mapleader = " "
 -- Set keymap group prefixes
 local fileleader = "<leader>f"
 
--- Disable arrow keys and backspace to aid learning 
+-- Disable arrow keys and backspace to aid learning
 vim.keymap.set({"i", "n"}, "<Right>", "")
 vim.keymap.set({"i", "n"}, "<Up>", "")
 vim.keymap.set({"i", "n"}, "<Left>", "")
 vim.keymap.set({"i", "n"}, "<Down>", "")
 
--- General keymaps  
-vim.keymap.set("i", "jk", "<ESC>")    
+-- General keymaps
+vim.keymap.set("i", "jk", "<ESC>")
 
 vim.keymap.set("n", "<leader>nh", ":nohl<CR>")
 
@@ -26,11 +26,13 @@ vim.keymap.set("n", "<leader>ex", ":Ex<CR>")
 
 -- Keymap for changing text wrapping with <A-z>
 vim.keymap.set("n", "<A-z>", function()
-    if vim.opt.wrap:get() == true then 
+    if vim.opt.wrap:get() == true then
         print("set wrap false");
-        vim.opt.wrap = false 
-    else 
+        vim.opt.wrap = false
+    else
         print("set wrap true");
         vim.opt.wrap = true
+        vim.opt.linebreak = true
+        vim.opt.breakindent = true
     end
 end)
